5c37da38901f11fdb3a31cf8c60e57507d39b610
Arash Rouhani <miffoljud@gmail.com>
2012-04-18 19:29:43 +0200

    Add "Läs mer..." [close #33]

--- Model/Post.hs
+++ Model/Post.hs
@@ -2,12 +2,20 @@ module Model.Post where
 
 import Prelude
 import Model.Persist
-import Data.Text (Text)
+import Data.Text (Text, strip, pack)
 import Data.Monoid (mconcat)
 import Yesod.Markdown
+
+-- This should ideally be provided by markdown but isn't
+unMarkdown :: Markdown -> Text
+unMarkdown (Markdown s) = pack s
 
 postContent :: Post -> Markdown
 postContent post = mconcat $ [postTeaser post | postSumem post] ++ [postBody post]
 
 postIsEdited :: Post -> Bool
 postIsEdited post = postCreated post /= postEdited post
+
+moreToRead :: Post -> Bool
+moreToRead post = let f = strip . unMarkdown . ($ post)
+                  in  f postTeaser /= f postContent
