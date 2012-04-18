85de1b04b5d4079d5254273bae175e74b96a0afc
Arash Rouhani <miffoljud@gmail.com>
2012-04-18 19:30:13 +0200

    Haddock for Model.Post

--- Model/Post.hs
+++ Model/Post.hs
@@ -10,12 +10,16 @@ import Yesod.Markdown
 unMarkdown :: Markdown -> Text
 unMarkdown (Markdown s) = pack s
 
+-- | The "full" content of the article, including the teaser if concat
 postContent :: Post -> Markdown
 postContent post = mconcat $ [postTeaser post | postSumem post] ++ [postBody post]
 
+-- | Yes if it's ever been edited
 postIsEdited :: Post -> Bool
 postIsEdited post = postCreated post /= postEdited post
 
+-- | Yes if the the content of the teaser is less than the content
+-- of whole article.
 moreToRead :: Post -> Bool
 moreToRead post = let f = strip . unMarkdown . ($ post)
                   in  f postTeaser /= f postContent
