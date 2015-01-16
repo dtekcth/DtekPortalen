d982304076190b4cbaab2753f63cbdea34db2cbf
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2015-01-16 16:58:49 +0100

    Use Paths module

--- Settings/StaticFiles.hs
+++ Settings/StaticFiles.hs
@@ -1,17 +1,25 @@
 module Settings.StaticFiles where
 
-import Prelude (IO)
-import Yesod.Static
+import           Control.Monad (return)
+import           Data.Default (def)
+import           Language.Haskell.TH (Q, Exp, Name)
+import           Prelude (IO)
+import           Yesod.Static
 import qualified Yesod.Static as Static
-import Settings (staticDir)
-import Settings.Development
-import Language.Haskell.TH (Q, Exp, Name)
-import Data.Default (def)
+
+import           Paths_DtekPortalen
+import           Settings (staticDir)
+import           Settings.Development
+
 
 -- | use this to create your static file serving site
 staticSite :: IO Static.Static
-staticSite = if development then Static.staticDevel staticDir
-                            else Static.static staticDir
+staticSite =
+  do staticDir <- getDataFileName "static"
+     (if development
+         then Static.staticDevel
+         else Static.static) staticDir
+
 
 -- | This generates easy references to files in the static directory at compile time,
 -- giving you compile-time verification that referenced files exist.
